2df6a0bcb0fe621e7d329a77aa5ab1e2f22ab8a3
Nick Marshall <nickmarshall3258@gmail.com>
2025-08-02 18:42:47 -0500

    added colors to pivot and upon completion

--- quicksort.js
+++ quicksort.js
@@ -25,8 +25,8 @@ async function quicksort(myArray, offset) {
       visualArrayL.push(visualArrayR.splice(i - counter, 1)[0]);
       counter++;
 
-      await visibleArray(visualArrayL.concat([pivot]).concat(visualArrayR), offset);
-      await sleep(5000/maxnum);
+      await visibleArray(visualArrayL.concat([pivot]).concat(visualArrayR), offset, counter);
+      await sleep(3000/maxnum);
     } else {
       arr2.push(myArray[i]);
     }
@@ -37,25 +37,67 @@ async function quicksort(myArray, offset) {
   return leftSorted.concat([pivot]).concat(rightSorted);
 }
 
-async function visibleArray(currentArray, offset) {
+async function visibleArray(currentArray, offset, counter) {
   const container = document.querySelector(".bar-box");
 
   container.innerHTML = "";
 
   globalArray.splice(offset, currentArray.length, ...currentArray);
 
-  globalArray.forEach(num => {
+  globalArray.forEach((num, i) => {
     const div = document.createElement("div");
     div.classList.add("bar");
-    div.style.height = `${(num/maxnum)*100}%`;
-    div.style.width = `${(maxnum)}%`;
+    div.style.height = `${(num / maxnum) * 100}%`;
+    div.style.width = `${(100 / globalArray.length)}%`;
+
+    if (i === offset + counter - 1) {
+        div.style.backgroundColor = "red";
+    }
+
     container.appendChild(div);
-  });
+    });
+}
+
+async function completeArray() {
+    const beforeLength = maxnum / 10;
+
+    for (let i = maxnum / 10; i < maxnum; i++) {
+        const container = document.querySelector(".bar-box");
+        container.innerHTML = "";
+
+        globalArray.forEach((num, index) => {
+            const div = document.createElement("div");
+            div.classList.add("bar");
+            div.style.height = `${(num / maxnum) * 100}%`;
+            div.style.width = `${(100 / globalArray.length)}%`;
+
+            if (index <= i && index >= i - beforeLength) {
+                div.style.backgroundColor = "green";
+            } else {
+                div.style.backgroundColor = "white";
+            }
+
+            container.appendChild(div);
+        });
+        await sleep(1000 / maxnum);
+    }
+    await sleep(5000 / maxnum);
+    const container = document.querySelector(".bar-box");
+    container.innerHTML = "";
+
+    globalArray.forEach((num) => {
+    const div = document.createElement("div");
+    div.classList.add("bar");
+    div.style.height = `${(num / maxnum) * 100}%`;
+    div.style.width = `${(100 / globalArray.length)}%`;
+
+    container.appendChild(div);
+    });
 }
 
 let globalArray = [];
 
-const maxnum = 500;
+const maxnum = 1000;
 
 document.getElementById("startDemoButton").addEventListener("click", async function() {
   globalArray = [];
@@ -63,4 +105,6 @@ document.getElementById("startDemoButton").addEventListener("click", async funct
   globalArray = Array.from(myArray);
 
   await quicksort(myArray, 0);
+  await completeArray();
+
 });
